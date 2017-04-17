32744b1d8749954a7a2564fa1f9ec5fc700d5523
Sean O'Dell <amsean.odell@gmail.com>
2017-04-17 16:10:25 -0400

    setup

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,10 +1,10 @@
 /**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
+*
+* App
+*
+* This component is the skeleton around the actual pages, and should only
+* contain code that should be seen on all pages. (e.g. navigation bar)
+*/
 
 import React from 'react';
 import Helmet from 'react-helmet';
@@ -15,29 +15,29 @@ import Footer from 'components/Footer';
 import withProgressBar from 'components/ProgressBar';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
+max-width: calc(768px + 16px * 2);
+margin: 0 auto;
+display: flex;
+min-height: 100%;
+padding: 0 16px;
+flex-direction: column;
 `;
 
 export function App(props) {
-  return (
-    <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-        meta={[
-          { name: 'description', content: 'A React.js Boilerplate application' },
-        ]}
-      />
-      <Header />
-      {React.Children.toArray(props.children)}
-      <Footer />
-    </AppWrapper>
-  );
+    return (
+      <AppWrapper>
+        <Helmet
+          titleTemplate="%s - Sean O'Dell"
+          defaultTitle="Sean O'Dell"
+          meta={[
+            { name: 'description', content: 'A simple test portfolio' },
+          ]}
+          />
+        <Header />
+        {React.Children.toArray(props.children)}
+        <Footer />
+      </AppWrapper>
+    );
 }
 
 App.propTypes = {
